ff603d75041098b6abe6e909e013811ab490b9f1
fishnchipa <wilsoncaox@gmail.com>
2023-12-24 08:24:20 +1100

    changes

--- demo/src/main/java/com/example/Controllers/ViewController.java
+++ demo/src/main/java/com/example/Controllers/ViewController.java
@@ -8,7 +8,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
@@ -21,13 +20,15 @@ public class ViewController {
     @FXML 
     private ImageView screen;
 
-    @FXML
-    private Button exit;
+
+    public ViewController() {
+        Stage stage = (Stage) screen.getScene().getWindow();
+        stage.setFullScreen(true);
+    }
 
     public void initialize() {
         Scene scene = ScreenController.getScene();
-        Stage stage = (Stage) exit.getScene().getWindow();
-        stage.setFullScreen(true);
+
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
         
             @Override
