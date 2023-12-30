065b8d4feff33381f6579e200ee142dd74e99b0a
fishnchipa <wilsoncaox@gmail.com>
2023-12-31 02:23:57 +1100

    full screen on enter false

--- demo/src/main/java/com/example/Controllers/CaptureController.java
+++ demo/src/main/java/com/example/Controllers/CaptureController.java
@@ -1,8 +1,20 @@
 package com.example.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class CaptureController {
+    @FXML
+    private Pane Body;
+
+    public void initialize() {
+        Stage stage = (Stage) Body.getScene().getWindow();
+        stage.setFullScreen(false);
+        stage.setMaximized(false);
+    }
+
     public void exit(ActionEvent event) {
         System.exit(0);
     }
